4538e47202843bd16aff188e7c22180b541a9a61
Sarjerao Gund <sarjeraogund@gmail.com>
2019-11-05 21:44:32 +0530

    BAse64 File upload code includee in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observer, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  base64Image: any;
+
   constructor() { }
 
   ngOnInit() {
+    // for testing you can add any image url here or get dynamically  from other methods as you require
+    let imageUrl = 'assets/img/intro-carousel/4.jpg';
+
+    this.getBase64ImageFromURL(imageUrl).subscribe(base64data => {    
+      console.log(base64data);
+      // this is the image as dataUrl
+      this.base64Image = 'data:image/jpg;base64,' + base64data;
+    });
   }
+
+  getBase64ImageFromURL(url: string) {
+    return Observable.create((observer: Observer<string>) => {
+      // create an image object
+      let img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.src = url;
+      if (!img.complete) {
+          // This will call another method that will create image from url
+          img.onload = () => {
+          observer.next(this.getBase64Image(img));
+          observer.complete();
+        };
+        img.onerror = (err) => {
+           observer.error(err);
+        };
+      } else {
+          observer.next(this.getBase64Image(img));
+          observer.complete();
+      }
+    });
+ }
+
+ getBase64Image(img: HTMLImageElement) {
+  // We create a HTML canvas object that will create a 2d image
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  var ctx = canvas.getContext("2d");
+  // This will draw image    
+  ctx.drawImage(img, 0, 0);
+  // Convert the drawn image to Data URL
+  var dataURL = canvas.toDataURL("image/png");
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
 
 }
